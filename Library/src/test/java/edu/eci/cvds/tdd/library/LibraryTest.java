2b8d86c65c843ff49dd2bd2c7e801115d2d21a45
juanescan <juanestebancancelado63@gmail.com>
2024-08-29 15:40:14 -0500

    creacion de la primera prueba addBook

--- Library/src/test/java/edu/eci/cvds/tdd/library/LibraryTest.java
+++ Library/src/test/java/edu/eci/cvds/tdd/library/LibraryTest.java
@@ -1,5 +1,15 @@
-import org.junit.*;
+package edu.eci.cvds.tdd.library;
+
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
 import java.util.Map;
+
+import edu.eci.cvds.tdd.library.Library;
+import edu.eci.cvds.tdd.library.book.Book;
+import edu.eci.cvds.tdd.library.loan.Loan;
+import edu.eci.cvds.tdd.library.user.User;
 
 public class LibraryTest{
     private Library library;
@@ -12,6 +22,11 @@ public class LibraryTest{
     @Test
 
     public void testAddBook(){
+        String tittle = "Cien años de soledad";
+        String author = "Gabriel Garcia Marquez";
+        String isbn = "8950";
         
+        Book book = new Book(tittle, author,isbn);
+        assertTrue(library.addBook(book));
     }
 }
